6e922a92767af17f12a262479b636a1a4834e1be
Thibaut F <fabre.thibaut@gmail.com>
2025-08-27 21:03:50 +0200

    [FIX] update API base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://backend:8080';
+const API_BASE_URL = process.env.VUE_API_BASE_URL || 'http://127.0.0.1:8080';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
