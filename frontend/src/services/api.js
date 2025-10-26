8c6d0ea1ffa295e80191e57102172f0128f19ab4
Thibaut F. <fabre.thibaut@gmail.com>
2025-10-26 13:18:51 +0100

    [FIX] routes api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VUE_APP_API_BASE_URL
+const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || '/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
